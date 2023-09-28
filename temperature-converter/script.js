a0f2a553457f2c2f4f6ab08f9e3202fc74fa1c60
TanyaChaudhary010704 <131010486+TanyaChaudhary010704@users.noreply.github.com>
2023-09-28 22:13:28 +0530

    temperature converter added

---
+++ temperature-converter/script.js
@@ -0,0 +1,15 @@
+const celcius = document.querySelector('#celcius'),
+  farenheit = document.querySelector('#farenheit');
+window.addEventListener("load", () => celcius.focus());
+celcius.addEventListener("input", () => {
+  farenheit.value = ((celcius.value * 9) / 5 + 32).toFixed(2);
+  if (!celcius.value) {
+    farenheit.value =""
+  }
+  });
+farenheit.addEventListener("input", () => {
+  celcius.value = ((farenheit.value -32) * 5 / 9).toFixed(2);
+  if (!farenheit.value) {
+    celcius.value =""
+  }
+  });
